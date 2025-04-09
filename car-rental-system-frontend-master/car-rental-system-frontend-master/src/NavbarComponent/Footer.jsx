56a9326d81ba8d807a217bd4f51f09d795f1ecee
sairaskar <sairaskar26@gmail.com>
2025-04-09 19:35:14 +0530

    add files

---
+++ car-rental-system-frontend-master/car-rental-system-frontend-master/src/NavbarComponent/Footer.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div>
+      <div class="container my-5">
+        <footer class="text-center text-lg-start">
+          <div class="container-fluid p-4 pb-0">
+            <section class="">
+              <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h4 class="text-uppercase text-color">
+                    <i>Car Rental System</i>
+                  </h4>
+
+                  <p className="header-logo-color">
+                    Welcome to our world of boundless exploration. Embrace the
+                    freedom to wander, discover, and make unforgettable
+                    memories. Let's embark on this journey together. Welcome to
+                    our community!
+                  </p>
+                </div>
+                </div>
+            </section>
+
+            <hr class="mb-4" />
+
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3 custom-bg-text">
+                  <b>Login from here</b>
+                </span>
+                <Link to="/user/login" class="active">
+                  <button
+                    type="button"
+                    class="btn btn-outline-light btn-rounded bg-color custom-bg-text"
+                  >
+                    <b> Log in</b>
+                  </button>
+                </Link>
+              </p>
+            </section>
+
+            <hr class="mb-4" />
+          </div>
+
+          <div class="text-center text-color">
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
